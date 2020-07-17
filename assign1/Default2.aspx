5e75a0669ddce81357e351f358d6762f24f4313a
puiind <62104708+puiind@users.noreply.github.com>
2020-07-17 18:49:32 -0400

    test
    test asp project

---
+++ assign1/Default2.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default2.aspx.cs" Inherits="Default2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>assignment#1</title>
+    <style>
+        center{
+  align-content: center;
+  margin: auto;
+  justify-content: center;
+  display: flex;
+}
+    </style>
+</head>
+<body>
+   <center>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Label1"></asp:Label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label2" runat="server" Text="Label2"></asp:Label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label3" runat="server" Text="Label3"></asp:Label>
+        </div>
+        <br />
+        <div>
+            <h4>Please select a date, it will display on Label1</h4>
+        <asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged"></asp:Calendar>
+        </div>
+
+        <div>
+            <h4>Please select degree(s) then press the button, the degree(s) will display on Label2</h4>
+            <asp:CheckBoxList ID="CheckBoxList1" runat="server">
+               <asp:ListItem>Bachelor</asp:ListItem>
+               <asp:ListItem>Masters</asp:ListItem>
+            </asp:CheckBoxList>
+            
+        <br />
+             <h4>Please type more than 2 digits and select a operation then press the button, the result will display on Label3</h4>
+        <asp:TextBox ID="TextBox1" runat="server" Type="Number"></asp:TextBox> 
+        <br />
+       
+        <asp:RadioButton ID="RadioButton1" runat="server" Text="Half" GroupName="Calculate" />
+        <br />
+        <asp:RadioButton ID="RadioButton2" runat="server" Text="Double" GroupName="Calculate" />
+        <br />
+        <asp:RadioButton ID="RadioButton3" runat="server" Text="Triple" GroupName="Calculate" />
+        
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+        </p>
+        </div>
+    </form>
+           <h5> Name : Podjamarn Phonyothee Section: CIT250-ME01 </h5>
+    </center>
+</body>
+</html>
---
+++ assign1/Default2.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    
+    }
+
+    protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+    {
+        Label1.Text = Calendar1.SelectedDate.ToString("MM/dd/yyyy");
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {  
+        //checkBoxList
+        int chkCount = 0;
+
+        for (int x = 0; x < CheckBoxList1.Items.Count; x++)
+        {
+            if (CheckBoxList1.Items[x].Selected)
+                chkCount++;
+        }
+
+
+        if (chkCount > 0)
+        {
+            Label2.Text = " ";
+            for (int i = 0; i <= CheckBoxList1.Items.Count - 1; i++)
+            {
+                if (CheckBoxList1.Items[i].Selected == true)
+                {
+                    Label2.Text += CheckBoxList1.Items[i].Value + " ";
+                }
+            }
+        }
+        else
+        {
+            Label2.Text = "Label2";
+        }
+        
+
+
+        //RadioButton
+
+        if (TextBox1.Text.Length == 0)
+        {   
+            if (RadioButton1.Checked == true)
+            {
+                Label3.Text = "Label3";
+            }
+            else if (RadioButton2.Checked == true)
+            {
+                Label3.Text = "Label3";
+            }
+            else if (RadioButton3.Checked == true)
+            {
+                Label3.Text = "Label3";
+            }
+            else
+            {
+                Label3.Text = "Label3";
+            }
+
+        }
+
+        else if (TextBox1.Text.Length < 2)
+        {
+            if (RadioButton1.Checked == true)
+            {
+                Label3.Text = "Label3";
+            }
+            else if (RadioButton2.Checked == true)
+            {
+                Label3.Text = "Label3";
+            }
+            else if (RadioButton3.Checked == true)
+            {
+                Label3.Text = "Label3";
+            }
+            else
+            {
+                Label3.Text = TextBox1.Text;
+            }
+        }
+
+        else if (TextBox1.Text.Length >= 2)
+        {
+            double inputValue = Convert.ToDouble(TextBox1.Text);
+
+            if (RadioButton1.Checked == true)
+            {
+                Label3.Text = (inputValue / 2).ToString();
+            }
+            else if (RadioButton2.Checked == true)
+            {
+                Label3.Text = (inputValue * 2).ToString();
+            }
+            else if (RadioButton3.Checked == true)
+            {
+                Label3.Text = (inputValue *3).ToString();
+            }
+            else
+            {
+                Label3.Text = TextBox1.Text;
+            }
+        }
+
+    }
+
+ 
+}
+
+
